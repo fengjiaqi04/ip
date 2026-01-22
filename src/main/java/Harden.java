dc8b928198446b217bff1566c443a449c641de2c
Feng Jiaqi <2469723297yy@gmail.com>
2026-01-22 11:19:15 +0800

    Complete Level-2: add, list

--- src/main/java/Harden.java
+++ src/main/java/Harden.java
@@ -4,6 +4,9 @@ public class Harden {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello! I'm Harden");
         System.out.println("What can I do for you?");
 
@@ -13,8 +16,16 @@ public class Harden {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+
             } else {
-                System.out.println(input);
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
         }
     }
