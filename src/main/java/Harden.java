62972e886e6d0e19b2aa964399fcdfaddc4363dc
Feng Jiaqi <2469723297yy@gmail.com>
2026-01-22 11:10:12 +0800

    Complete Level-1: echo

--- src/main/java/Harden.java
+++ src/main/java/Harden.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Harden {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! I'm Harden");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
