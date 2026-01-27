de56cb2a3d71c79069546ecb0412d9d740e1fd18
Feng Jiaqi <2469723297yy@gmail.com>
2026-01-27 18:28:49 +0800

    Fix list command output

--- src/main/java/harden/ListCommand.java
+++ src/main/java/harden/ListCommand.java
@@ -8,7 +8,7 @@ public class ListCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         for (int i = 0; i < tasks.size(); i++) {
-            ui.showMessage((i + 1) + ".");
+            ui.showMessage((i + 1) + "." + tasks.getTask(i));
         }
     }
 }
--- src/main/java/harden/TaskList.java
+++ src/main/java/harden/TaskList.java
@@ -53,4 +53,9 @@ public class TaskList {
     public ArrayList<Task> getTasks() {
         return tasks;
     }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
 }
